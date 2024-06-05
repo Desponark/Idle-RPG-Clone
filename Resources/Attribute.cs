9e98672430a8f2add4e728dcd68e9bd6856c544c
Desponark <129955348+Desponark@users.noreply.github.com>
2024-06-05 11:29:22 +0200

    update attribute

--- Resources/Attribute.cs
+++ Resources/Attribute.cs
@@ -5,11 +5,47 @@ public partial class Attribute : Resource {
 	[Export]
 	public int Value = 1;
 
+	public virtual float ModMaxHp(float retVal) {
+		return retVal;
+	}
+
+	public virtual float ModMaxSp(float retVal) {
+		return retVal;
+	}
+
+	public virtual float ModMaxMp(float retVal) {
+		return retVal;
+	}
+
+	public virtual float ModHPReg(float retVal) {
+		return retVal;
+	}
+
+	public virtual float ModSPReg(float retVal) {
+		return retVal;
+	}
+
+	public virtual float ModMPReg(float retVal) {
+		return retVal;
+	}
+
 	public virtual float ModAtk(float retVal) {
 		return retVal;
 	}
 
 	public virtual float ModMagAtk(float retVal) {
+		return retVal;
+	}
+
+	public virtual float ModAcc(float retVal) {
+		return retVal;
+	}
+
+	public virtual float ModAtkSpd(float retVal) {
+		return retVal;
+	}
+
+	public virtual float ModMoveSpd(float retVal) {
 		return retVal;
 	}
 }
